c1e3ce90ef7d0a39d4cf7ada916ba2374e840803
GAudrey <gilmantaudrey.ag@gmail.com>
2020-12-17 16:39:39 +0100

    modified date and time, changing background, modified css

--- script.js
+++ script.js
@@ -1,29 +1,98 @@
+// let objDays = [
+//     {
+//         day: 'Lundi',
+//         value:1
+//     },
+//     {
+//         day: 'Mardi',
+//         value:2
+//     },
+//     {
+//         day: 'Mercredi',
+//         value:3
+//     },
+//     {
+//         day: 'Jeudi',
+//         value:4
+//     },
+//     {
+//         day: 'Vendredi',
+//         value:5
+//     },
+//     {
+//         day: 'Samedi',
+//         value:6
+//     },
+//     {
+//         day: 'Dimanche',
+//         value:0
+//     },
+// ];
+
+// let objMonths = [
+//     {
+//         month: 'janvier',
+//         value:0
+//     },
+//     {
+//         month: 'février',
+//         value:1
+//     },
+//     {
+//         month: 'mars',
+//         value:2
+//     },
+//     {
+//         month: 'avril',
+//         value:3
+//     },
+//     {
+//         month: 'mai',
+//         value:4
+//     },
+//     {
+//         month: 'juin',
+//         value:5
+//     },
+//     {
+//         month: 'juillet',
+//         value:6
+//     },
+//     {
+//         month: 'août',
+//         value:7
+//     },
+//     {
+//         month: 'septembre',
+//         value:8
+//     },
+//     {
+//         month: 'octobre',
+//         value:9
+//     },
+//     {
+//         month: 'novembre',
+//         value:10
+//     },
+//     {
+//         month: 'décembre',
+//         value:11
+//     },
+// ];
+
 const dateSection = document.getElementById('date-section');
 const timeSection = document.getElementById('time-section');
+
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 let now;
 
 function date(){
     now = new Date();
-    let day = now.getDay();
-    let currentDay;
-    let date = now.getDate();
-    let month = now.getMonth();
-    let currentMonth;
-    let fullyear = now.getFullYear();
+    let currentDate = now.toLocaleDateString(undefined, options);
+    dateSection.textContent = currentDate;
 
-    for (let d of objDays){
-        if (day == d.value){
-            currentDay = `${d.day}`;
-        }
-    }
-    for (let m of objMonths){
-        if (month == m.value){
-            currentMonth = `${m.month}`;
-        }
-    }
-
-    dateSection.textContent = `${currentDay}, ${date} ${currentMonth} ${fullyear}`
+    setTimeout(date, 1000);
 }
 date();
 
@@ -45,3 +114,14 @@ function clock(){
     setTimeout(clock, 1000)
 }
 clock();
+
+fetch('https://picsum.photos/v2/list?limit=30')
+.then(response => response.json())
+.then(images => {
+    function randomImg(){
+        let i = Math.floor(Math.random() * images.length);
+        document.body.style.backgroundImage = `url(${images[i].download_url})`;
+    }
+    randomImg();
+    setInterval(randomImg, 5000)
+})
